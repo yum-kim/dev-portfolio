c920e3b056beb92f73fa1f7255682094f70a5520
YumiKim <dwkimym93@gmail.com>
2022-10-27 23:43:48 +0900

    header 임시 텍스트 삭제

--- resume/src/components/AppLayout.jsx
+++ resume/src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AppLayout = ({ children }) => {
     return (
         <>
-            <header>고정영역</header>
+            <header></header>
             <main>
                 {children}
             </main>
